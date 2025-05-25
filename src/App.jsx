c244877a18da5e7d52ec4b7080b3346e79d5533e
kashif997-lab <kashifsatti6900@gmail.com>
2025-05-25 17:47:04 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import DailyExpenseTracker from "./components/DailyExpenseTracker";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -8,21 +9,24 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Router>
-     <Routes>
-  <Route path="/login" element={<Login />} />
-  <Route path="/signup" element={<Signup />} />
-  <Route
-    path="/dashboard"
-    element={
-      <ProtectedRoute>
-        <DailyExpenseTracker />
-      </ProtectedRoute>
-    }
-  />
-</Routes>
+      <Routes>
+        {/* ✅ Add this redirect */}
+        <Route path="/" element={<Navigate to="/login" />} />
 
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DailyExpenseTracker />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
